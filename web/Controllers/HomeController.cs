f6b3a7972ea7299447da8f5788e56933e2e61708
Han Dong <31230864+MrChuJiu@users.noreply.github.com>
2024-06-11 13:29:20 +0800

    Update HomeController.cs

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -31,6 +31,8 @@ namespace web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var test = "312312312h3jkhcvsdfiosdfusidourowqhreoiwqrioewuiouibcf";
+            test = "31231211111111111111111111111111111111111111111122222222222222222222233333333333333333333333333";
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
